f7ec609d310de0b588f0fdf923a878a03f61500a
axelhd <99671483+axelhd@users.noreply.github.com>
2024-01-07 19:03:07 +0100

    Update main.nim

--- main.nim
+++ main.nim
@@ -1,15 +1,22 @@
-import os, std/httpclient, osproc
+import os, std/httpclient, zippy/ziparchives, strutils
 
-let url = "http://127.0.0.1:8000/Liveries.exe"
-let localFilePath = "%USERPROFILE%\\Saved Games\\DCS.openbeta"
-# Create the directory if it doesn't exist
+let url = "http://corvina.adora.dk:8054/liveries/Liveries.zip"
+let UserProfile = getEnv("USERPROFILE")
+let localFilePath = UserProfile/"Saved Games"/"DCS.openbeta"/"Liveries.zip"
+let installPath = UserProfile/"Saved Games"/"DCS.openbeta"/"Liveries"
+
+echo localFilePath
 
 # Download the file
 var client = newHttpClient()
+sleep(5000)
 try:
+    echo "Downloading liveries"
     let content = client.getContent(url)
-    echo "1"
+    echo "Installing Liveries"
+    sleep(5000)
     let file = open(localFilePath, fmWrite)
+    sleep(5000)
     # Write the content to the file in chunks
     const ChunkSize = 10_000_000 # 10 MB chunk size
     var i = 0
@@ -18,16 +25,19 @@ try:
         file.write(content[i ..< endIdx])
         i = endIdx
     file.close()
+    sleep(5000)
     
 finally:
     client.close()
-    
-# Execute the file
-let args = ["-y"]
-echo "here"
-discard execProcess(localFilePath, args = args, options = {poUsePath, poStdErrToStdOut})
-echo "here"
 
-# Delete the file
+sleep(5000)
+
+echo "Extracting files"
+
+# Unzip the file
+extractAll(localFilePath, installPath)
+sleep(5000)
+
 removeFile(localFilePath)
 
+echo "Succesfully installed all liveries"
